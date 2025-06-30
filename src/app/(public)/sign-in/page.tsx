9b91303af06b9fd42289847bb71b15bd328ac0cb
ismasan1399 <TU_CORREO_DE_GITHUB>
2025-06-29 22:40:42 -0600

    cambio en el inicio de sesión

--- src/app/(public)/sign-in/page.tsx
+++ src/app/(public)/sign-in/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const SignIn = () => {
-  return <div>Sign In</div>
+  return (
+    <div>
+      Sign In
+      <p>isma</p>
+    </div>
+  )
 }
 
 export default SignIn
